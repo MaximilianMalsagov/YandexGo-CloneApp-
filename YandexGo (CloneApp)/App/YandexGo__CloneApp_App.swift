70e567ac843768f21f0479720eaf57bcdc07cb68
Maximilian <maxim.malsagov@mail.ru>
2023-02-24 15:18:34 +0300

    Initial Commit

---
+++ YandexGo (CloneApp)/App/YandexGo__CloneApp_App.swift
@@ -0,0 +1,17 @@
+//
+//  YandexGo__CloneApp_App.swift
+//  YandexGo (CloneApp)
+//
+//  Created by Максимилиан Мальсагов on 24.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct YandexGo__CloneApp_App: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ YandexGo (CloneApp)/New Group/ViewModel/Views/YandexMapViewRepresentable.swift
@@ -0,0 +1,8 @@
+//
+//  YandexMapViewRepresentable.swift
+//  YandexGo (CloneApp)
+//
+//  Created by Максимилиан Мальсагов on 24.02.2023.
+//
+
+import Foundation
