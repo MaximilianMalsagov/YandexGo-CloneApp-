d8fff154bc78c7b52487afbe82975cc06fc67707
Maximilian <maxim.malsagov@mail.ru>
2023-02-25 17:09:43 +0300

    integrating Mapkit

---
+++ YandexGo (CloneApp)/Core/LocationSearch/Views/LocationSearchActivationView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationSearchActivationView.swift
+//  YandexGo (CloneApp)
+//
+//  Created by Максимилиан Мальсагов on 25.02.2023.
+//
+
+import SwiftUI
+
+struct LocationSearchActivationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationSearchActivationView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationSearchActivationView()
+    }
+}
