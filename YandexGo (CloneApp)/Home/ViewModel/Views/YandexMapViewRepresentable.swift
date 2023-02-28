dfd9fdae61ea48b325bdb70ec64d301d3df37dd6
Maximilian <maxim.malsagov@mail.ru>
2023-02-28 10:43:47 +0300

    Generated route to the destination

--- YandexGo (CloneApp)/Home/ViewModel/Views/YandexMapViewRepresentable.swift
+++ YandexGo (CloneApp)/Home/ViewModel/Views/YandexMapViewRepresentable.swift
@@ -24,8 +24,10 @@ struct YandexMapViewRepresentable: UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
-        if let selectedLocation = locationViewModel.selectedLocation {
-            print("DEBUG: Selected location in map view \(selectedLocation)")
+        if let coordinate = locationViewModel.selectedLocationCoordinate {
+            context.coordinator.addAndSelectAnnotation(withCoordinate: coordinate)
+            context.coordinator.configurePolyline(withDestinationCoordinate: coordinate)
+
         }
     }
     
@@ -37,13 +39,15 @@ struct YandexMapViewRepresentable: UIViewRepresentable {
 extension YandexMapViewRepresentable {
     class MapCoordinator: NSObject , MKMapViewDelegate {
         let parent : YandexMapViewRepresentable
-        
+        var userLocationCoordinate: CLLocationCoordinate2D?
         init(parent: YandexMapViewRepresentable) {
             self.parent = parent
             super.init()
             
         }
         func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            
+            self.userLocationCoordinate = userLocation.coordinate
             let region = MKCoordinateRegion(
                 center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude,
                                                longitude: userLocation.coordinate.longitude),
@@ -54,6 +58,56 @@ extension YandexMapViewRepresentable {
             parent.mapView.setRegion(region, animated: true)
         }
         
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            let polyline = MKPolylineRenderer(overlay: overlay)
+            polyline.strokeColor = .systemBlue
+            polyline.lineWidth = 6
+            return polyline
+        }
+        
+        func addAndSelectAnnotation(withCoordinate coordinate: CLLocationCoordinate2D) {
+            
+            parent.mapView.removeAnnotations(parent.mapView.annotations)
+            
+            let anno = MKPointAnnotation()
+            anno.coordinate = coordinate
+            parent.mapView.addAnnotation(anno)
+            parent.mapView.selectAnnotation(anno, animated: true)
+            
+            parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
+        }
+            
+        func configurePolyline(withDestinationCoordinate coordinate: CLLocationCoordinate2D) {
+            
+            guard let userLocationCoordinate = self.userLocationCoordinate else { return }
+            
+            getDestinationRoute(from: userLocationCoordinate,
+                                to: coordinate) { route in
+                self.parent.mapView.addOverlay(route.polyline)
+            }
+            
+        }
+            func getDestinationRoute(from userLocation: CLLocationCoordinate2D,
+                                     to destination: CLLocationCoordinate2D,
+                                     completion: @escaping(MKRoute) -> Void) {
+                
+                let userPlacemark = MKPlacemark(coordinate: userLocation)
+                let destPlacemark = MKPlacemark(coordinate: destination)
+                let request = MKDirections.Request()
+                request.source = MKMapItem(placemark: userPlacemark)
+                request.destination = MKMapItem(placemark: destPlacemark)
+                
+                let directions = MKDirections(request: request)
+                directions.calculate { response , error in
+                    if let error = error {
+                        print("DEBUG \(error.localizedDescription)")
+                        return
+                    }
+                    guard let route = response?.routes.first else { return }
+                    completion(route)
+                }
+                
+            }
+        }
     }
-    
-}
+
